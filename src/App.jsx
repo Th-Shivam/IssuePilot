6968d2ffe8cb01d5791dca5ddacbe8142145dd23
th-shivam <dreamyshivam01@gmail.com>
2026-01-14 12:02:23 +0530

    feat: integrate authentication with AuthProvider and add login/community routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 import Header from './components/Header';
 import VideoBackground from './components/VideoBackground';
 import LandingPage from './pages/LandingPage';
 import ExplorePage from './pages/ExplorePage';
+import CommunityPage from './pages/CommunityPage';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 import './index.css';
 
 function App() {
   return (
-    <div className="app-main">
-      <VideoBackground />
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/explore" element={<ExplorePage />} />
-        </Routes>
-      </main>
-    </div>
+    <AuthProvider>
+      <div className="app-main">
+        <VideoBackground />
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/community" element={<CommunityPage />} />
+            <Route
+              path="/explore"
+              element={
+                <ProtectedRoute>
+                  <ExplorePage />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </main>
+      </div>
+    </AuthProvider>
   );
 }
 
