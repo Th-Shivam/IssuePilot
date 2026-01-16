e5c2128cf60228232a67f2b2f1d33e69ab5b5cb7
th-shivam <dreamyshivam01@gmail.com>
2026-01-16 18:35:05 +0530

    feat: integrate google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from "react-ga4";
 import App from "./App";
 import "./index.css";
+import AnalyticsTracker from "./components/AnalyticsTracker";
+
+// Initialize GA4
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+if (GA_MEASUREMENT_ID) {
+  ReactGA.initialize(GA_MEASUREMENT_ID);
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <AnalyticsTracker />
       <App />
     </BrowserRouter>
   </React.StrictMode>
